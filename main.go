53e815ab7d8302a3a588e768185b2a067e0aa15f
Eran Chetz <eran.chetztroni@algolia.com>
2017-05-01 12:25:00 +0200

    add error handling

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 )
 
 func main() {
 	// Generator will create a new instance with an array of startup name and an array with ~5000 nouns
-	gen, _ := New()
+	gen, err := New()
+	if err != nil {
+		log.Fatalln("could not create generator", err)
+	}
 
 	// Build tweet
 	tweetBody := fmt.Sprintln(gen.startUps[rand.Intn(len(gen.startUps))], "for", gen.words[rand.Intn(len(gen.words))])
