4d9555b8152537c00175d613b30248c2d203a028
Eran Chetz <eran.chetztroni@algolia.com>
2017-05-05 15:04:02 +0200

    Revert "use top startups from all the world!"
    This reverts commit 02c0546ef5867278b5d709711b1e710c88ff09e3.

--- generator.go
+++ generator.go
@@ -27,7 +27,7 @@ func init() {
 
 func New() (*Generator, error) {
 	gen := new(Generator)
-	doc, err := goquery.NewDocument("http://www.startupranking.com/top")
+	doc, err := goquery.NewDocument("http://www.startupranking.com/top/united-states")
 	if err != nil {
 		return nil, err
 	}
